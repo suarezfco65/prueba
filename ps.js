0cf4432ed93dd5ee11d15f3b8ab6b0a288c208cd
Francisco Suarez <56086678+suarezfco65@users.noreply.github.com>
2025-03-05 07:41:21 -0400

    Update ps.js

--- ps.js
+++ ps.js
@@ -6676,6 +6676,7 @@ ps.push({	n	:	`Susana Ysso`,			r	:	`ESTUDIANTE 2`,	a	:	`2023`,	e	:	``,	p	:	``	})
 ps.push({	n	:	`Esperanza Moyano Conde`,	r	:	`ESTUDIANTE 3`,	a	:	`2023`,	e	:	``,	p	:	``	});
 ps.push({	n	:	`Veronica Toro`,		r	:	`FRTA VT`,	a	:	``,	e	:	``,	p	:	``	});
 ps.push({	n	:	`Virtudes Jaen`,		r	:	`ESTUDIANTE 4`,	a	:	`2025`,	e	:	``,	p	:	``	});
+ps.push({	n	:	`Natividad Martin`,		r	:	`ESTUDIANTE 5`,	a	:	`2025`,	e	:	``,	p	:	``	});
 
 
 
